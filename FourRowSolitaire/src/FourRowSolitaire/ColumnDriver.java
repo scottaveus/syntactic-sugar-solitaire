f1c0c286720facfb0d75f59ee7eed822855ba2fb
Sean Scott <sean.scott894@gmail.com>
2015-04-12 10:21:33 -0700

    ColumnDriver.java (Mark II)
    Better code coverage (90.4% infected with eclemma)

--- FourRowSolitaire/src/FourRowSolitaire/ColumnDriver.java
+++ FourRowSolitaire/src/FourRowSolitaire/ColumnDriver.java
@@ -2,7 +2,7 @@ package FourRowSolitaire;
 
 public class ColumnDriver 
 {
-    public static void main(String[] args)
+    private static void testEmptyPile()
     {
         Card nullCard = null;
         Card invalidCard = new Card("Spades", 11, 1, 11); // Jack of Spades
@@ -45,6 +45,101 @@ public class ColumnDriver
         {
             System.out.println("Attempting to push valid card resulted in NullPointerException");
         }
+    }
+    
+    private static void testNonEmptyPile()
+    {
+        Column col = new Column();
+        Card startCard = new Card("Hearts", 13, 1, 13); // King of Hearts
+        col.push(startCard);
+
+        Card nullCard = null;
+        Card invalidCard = new Card("Spades", 11, 1, 11); // Jack of Spades
+        Card validCard = new Card("Spades", 12, 1, 12); // Queen of Spades
+
+        
+        // Null card
+        try
+        {
+            Card resultCard = col.push(nullCard);
+            
+            System.out.println(resultCard.toString());
+        }
+        catch (NullPointerException e)
+        {
+            System.out.println("Attempting to push null card resulted in NullPointerException");
+        }
+        
+        // Invalid card
+        try
+        {
+            Card resultCard = col.push(invalidCard);
+            
+            System.out.println(resultCard.toString());
+        }
+        catch (NullPointerException e)
+        {
+            System.out.println("Attempting to push invalid card resulted in NullPointerException");
+        }
+        
+        // Valid card
+        try
+        {
+            Card resultCard = col.push(validCard);
+            
+            System.out.println(resultCard.toString());
+        }
+        catch (NullPointerException e)
+        {
+            System.out.println("Attempting to push valid card resulted in NullPointerException");
+        }
+    }
+    
+    private static void testValidMove()
+    {
+        Column emptyCol = new Column();
+        Card validCard = new Card("Hearts", 13, 1, 13); // King of Hearts
+        boolean result;
+        
+        result = emptyCol.isValidMove(validCard);
+        
+        if (result == true)
+        {
+            System.out.println("Passing King of Hearts to empty pile is a VALID move");
+        }
+        else
+        {
+            System.out.println("Passing King of Hearts to empty pile is an INVALID move");
+        }
+    }
+    
+    private static void testInvalidMove()
+    {
+        Column col = new Column();
+        Card validCard = new Card("Hearts", 13, 1, 13); // King of Hearts
+        Card invalidCard = new Card("Spades", 1, 1, 1); // Ace of Spades
+        boolean result;
+        
+        col.push(validCard);
+        result = col.isValidMove(invalidCard);
+        
+        if (result == true)
+        {
+            System.out.println("Passing Ace of Spades to pile is a VALID move");
+        }
+        else
+        {
+            System.out.println("Passing Ace of Spades to pile is an INVALID move");
+        }
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        testEmptyPile();
+        testNonEmptyPile();
+        testValidMove();
+        testInvalidMove();
     }
 
 }
