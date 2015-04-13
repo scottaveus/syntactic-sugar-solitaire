22371033e38a3109455848769097444d4242bbce
Sean Scott <sean.scott894@gmail.com>
2015-04-12 19:00:42 -0700

    lel

---
+++ FourRowSolitaire/src/FourRowSolitaire/SolitairePanelDriver.java
@@ -0,0 +1,12 @@
+package FourRowSolitaire;
+
+public class SolitairePanelDriver 
+{
+    public static void main(String[] args)
+    {
+        SolitairePanel sp = new SolitairePanel();
+        
+        sp.changeBackground(1);
+    }
+    
+}
