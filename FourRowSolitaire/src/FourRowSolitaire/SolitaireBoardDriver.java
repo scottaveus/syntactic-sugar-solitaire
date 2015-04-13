cfb2f1cdf89ee24a4e795f98fd1ffeab70f35564
Sean Scott <sean.scott894@gmail.com>
2015-04-12 20:12:03 -0700

    check

--- FourRowSolitaire/src/FourRowSolitaire/SolitaireBoardDriver.java
+++ FourRowSolitaire/src/FourRowSolitaire/SolitaireBoardDriver.java
@@ -2,7 +2,7 @@ package FourRowSolitaire;
 
 public class SolitaireBoardDriver 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {
         SolitaireBoard board = new SolitaireBoard();
         
