286e21f40ca63200dfc7b2cd70f0554de6edd8ea
Sean Scott <sean.scott894@gmail.com>
2015-04-12 20:11:09 -0700

    SolitaireBoard, at least what we can test

--- FourRowSolitaire/src/FourRowSolitaire/SolitaireBoardDriver.java
+++ FourRowSolitaire/src/FourRowSolitaire/SolitaireBoardDriver.java
@@ -4,7 +4,59 @@ public class SolitaireBoardDriver
 {
     public static void main(String[] args)
     {
-        // to do
+        SolitaireBoard board = new SolitaireBoard();
+        
+        board.createBoard(null);
+        
+        board.newGame(0);
+        
+        board.setAppearance(0, 0);
+        
+        board.getDrawCount();
+        
+        board.setDrawCount(0);
+        
+        board.getNewDrawCount();
+        
+        board.setNewDrawCount(0);
+        
+        board.getDeckNumber();
+        
+        board.setDeckNumber(0);
+        
+        board.getBackgroundNumber();
+        
+        board.setBackgroundNumber(0);
+        
+        board.getTimerNextGameStatus();
+        
+        board.getTimerStatus();
+        
+        board.setTimerStatus(0);
+        
+        board.getWinAnimationStatus();
+        
+        board.setWinAnimationStatus(0);
+        
+        board.getWinSoundsStatus();
+        
+        board.setWinSoundsStatus(0);
+        
+        board.getDifficulty();
+        
+        board.setDifficulty(0);
+        
+        board.getNewDifficulty();
+        
+        board.setNewDifficulty(0);
+        
+        board.setDeckThroughs(0);
+        
+        board.undoMove();
+        
+        board.getHint();
+        
+        
     }
 
 }
